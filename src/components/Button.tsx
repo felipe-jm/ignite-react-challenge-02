d5ea23ceeb15892180506a695f275fea97301198
Felipe Jung <felipemattoseu@gmail.com>
2021-03-16 09:06:25 -0400

    refactor: componentize application

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,17 @@
-import { Icon } from './Icon';
+import { Icon } from "./Icon";
 
-import '../styles/button.scss';
-import { ButtonHTMLAttributes } from 'react';
+import "../styles/button.scss";
+import { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+type ButtonProps = {
   title: string;
-  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  iconName: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
   selected: boolean;
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
-  return (
-    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
-      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
-      {title}
-    </button>
-  );
-}
+export const Button = ({ iconName, title, selected, ...rest }: ButtonProps) => (
+  <button type="button" {...(selected && { className: "selected" })} {...rest}>
+    <Icon name={iconName} color={selected ? "#FAE800" : "#FBFBFB"} />
+    {title}
+  </button>
+);
